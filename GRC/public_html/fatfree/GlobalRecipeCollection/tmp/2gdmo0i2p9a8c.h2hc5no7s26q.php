8c870b6c854d396a5154a087788800c56189f9e0
jingyichu <agnochu@gmail.com>
2021-02-25 17:51:51 +0800

    make alias for local complile

---
+++ GRC/public_html/fatfree/GlobalRecipeCollection/tmp/2gdmo0i2p9a8c.h2hc5no7s26q.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title><?= ($html_title) ?></title>
+    <link rel="stylesheet" type="text/css" href="static/css/index.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200;500;700&display=swap" rel="stylesheet">
+</head>
+<body style= "background: #EBEEF2">
+<header>
+    <div class="logo_div">
+        <img  class="logo" src="static/img/Logo_797 KB.png">
+    </div>
+    <div class="menu">
+        <ul class="list">
+            <li><a href="<?= ($BASE) ?>/projectreport">REPORT</a></li>
+            <li><a href="<?= ($BASE) ?>/login">LOG IN</a></li>
+            <li><a href="<?= ($BASE) ?>/creatrecipe">CREAT RECIPE</a></li>
+            <li><a href="<?= ($BASE) ?>/viewrecipe">VIEW RECIPE</a></li>
+            <li class="active"><a href="<?= ($BASE) ?>/"><u><b>HOME</b></u></a></li>
+        </ul>
+    </div>
+</header>
+
+    <div class="title">
+<!--        <br><br><br><br><br><br><br><br><br><br>-->
+
+<!--        <br><br><br><br><br><br><br><br><br><br>-->
+
+        <left><h1> Global Recipe collectin </h1></left>
+
+        <h2>Introduction</h2>
+
+        <p>
+
+        </p>
+
+        <br><br><br><br><br>
+
+
+
+
+        <div class=
+                     </div>
+
+
+
+
+
+        </div>
+</body>
+
+
+</html>
+
+
